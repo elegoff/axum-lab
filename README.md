81bf89f00d0cedc2551338426cae50d903684812
Eric Le Goff <elegoff@protonmail.com>
2024-02-06 12:08:19 +0100

    add http client unit test 0527

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 -  cargo run
 - browse http://localhost:8080/hello
 
+alias cw='cargo watch -q -c -w src/ -x run'
+cargo watch -q -c -w tests/ -x "test -q quick_dev -- --nocapture"
+
---
+++ tests/quick_dev.rs
@@ -0,0 +1,11 @@
+#![allow(unused)] //for beginning
+                  //
+use anyhow::Result;
+
+#[tokio::test]
+async fn quick_dev() -> Result<()> {
+    let base_url = "http://localhost:8080";
+    let hc = httpc_test::new_client(base_url)?;
+    hc.do_get("/hello").await?.print().await?;
+    Ok(())
+}
