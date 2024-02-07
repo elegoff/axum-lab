6102b2306cc662e592e8e7d7c6b1516a8ec52aaa
Eric Le Goff <elegoff@protonmail.com>
2024-02-07 14:33:26 +0100

    Merging routes

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,23 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new()
-        .route("/hello", get(handler_hello))
-        .route("/hello2/:name", get(handler_hello2));
+    let routes_all = Router::new().merge(routes_hello());
 
     // Server
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
     println!("->> LITENING on {:?}\n", listener.local_addr());
 
-    axum::serve(listener, routes_hello.into_make_service())
+    axum::serve(listener, routes_all.into_make_service())
         .await
         .unwrap();
+}
+
+// Routes hello
+//
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
 }
 
 #[derive(Debug, Deserialize)]
