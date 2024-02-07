a8bc48703b9c97fcc0e7a37fd8d41cc3f01ccebb
Eric Le Goff <elegoff@protonmail.com>
2024-02-07 12:26:42 +0100

    use Path uri 1040

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)] //just for beginning
 
+use axum::extract::Path;
 use axum::extract::Query;
 use axum::response::Html;
 use axum::response::IntoResponse;
@@ -10,7 +11,9 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
+    let routes_hello = Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2));
 
     // Server
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
@@ -25,9 +28,15 @@ async fn main() {
 struct HelloParams {
     name: Option<String>,
 }
-
+// e.g /hello?name=John
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     let name = params.name.as_deref().unwrap_or("World");
     println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
     Html(format!("Hello <strong>{name}</strong>"))
+}
+
+//e.g /hello/Jane
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+    Html(format!("Hello2 <strong>{name}</strong>"))
 }
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -5,7 +5,8 @@ use anyhow::Result;
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
     let base_url = "http://localhost:8080";
+
     let hc = httpc_test::new_client(base_url)?;
-    hc.do_get("/hello?name=Vincent").await?.print().await?;
+    hc.do_get("/hello2/Vincent").await?.print().await?;
     Ok(())
 }
