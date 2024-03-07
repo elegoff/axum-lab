a6147e9aaf25f469c719c15853b4934b65ddfeff
Eric Le Goff <elegoff@protonmail.com>
2024-03-07 16:22:06 +0100

    Correlate client and server errors

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,13 @@ use axum::middleware::map_response;
 use axum::response::Html;
 use axum::response::{IntoResponse, Response};
 use axum::routing::{get, get_service};
-use axum::Router;
+use axum::{Json, Router};
 use serde::Deserialize;
+use serde_json::json;
 use tokio::net::TcpListener;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
 mod ctx;
 mod error;
@@ -54,6 +56,34 @@ async fn main() -> Result<()> {
 
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+
+    let uuid = Uuid::new_v4();
+
+    //get the potential response error
+    //
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    //In case of error, buidl a specific response
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+            "error" : {
+            "type" : client_error.as_ref(),
+            "req_uuid" : uuid.to_string(),
+            }
+
+            });
+
+            println!("    ->> client_error_body: {client_error_body}");
+
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    //TODO : buid and log the server log line
+    //
+    println!("    ->> server log line - {uuid} - Error : {service_error:?}");
     println!();
     res
 }
